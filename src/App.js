b8aa42ca3fa8c4cd4ae521e20b7975c52f4c23ed
Somesh5723 <s2somesh237@gmail.com>
2023-08-13 23:37:13 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Dashboard />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-200 p-4 text-center">
+      <p>&copy; 2023 AI Product Dashboard. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
